92bf7cd5288e216faa26513ab65c81d7c99dd0fa
AzerDarkBlade@gmail.com <AzerDarkBlade@gmail.com@1e499364-ebf0-39a1-8228-1d92c139d8e0>
2011-09-22 06:47:15 +0000

    Repairing Terrain and Skyplane still has problem with selecting character

--- trunk/RayRayRay/src/BaseApplication.cpp
+++ trunk/RayRayRay/src/BaseApplication.cpp
@@ -63,7 +63,7 @@ bool BaseApplication::configure(void)
     {
         // If returned true, user clicked OK so initialise
         // Here we choose to let the system create a default rendering window by passing 'true'
-        mWindow = mRoot->initialise(true, "RayRayRay Render Window");
+        mWindow = mRoot->initialise(true, "Ray~Ray~Ray");
 
         // Let's add a nice window icon
 #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
